b1584323b5aeaa0a24d6bb427a19a8aa12b88501
Matt Pindell <pindell.m@gmail.com>
2016-05-31 15:17:49 -0600

    Update Merchant relationship to include invoices

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe Merchant do
   it { should have_many(:items) }
-  # it { should have_many(:invoices) }
+  it { should have_many(:invoices) }
   it { should validate_presence_of(:name) }
 end
