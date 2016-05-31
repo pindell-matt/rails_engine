0fd0ac4bda2b69b756dd25626129dd61d2b0f404
Matt Pindell <pindell.m@gmail.com>
2016-05-31 15:51:05 -0600

    Adjust spacing of assertions on each spec

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 describe InvoiceItem do
   it { should belong_to :item }
   it { should belong_to :invoice }
+  it { should validate_presence_of :quantity }
+  it { should validate_presence_of :unit_price }
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,4 +4,5 @@ describe Invoice do
   it { should belong_to :customer }
   it { should belong_to :merchant }
   it { should have_many :invoice_items }
+  it { should validate_presence_of :status }
 end
