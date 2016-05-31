f759cbac5916e429376f95c6fce716bb50b2aee9
Matt Pindell <pindell.m@gmail.com>
2016-05-31 17:22:28 -0600

    Add valid factory coverage to Merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,19 @@ describe Merchant do
   it { should have_many :items }
   it { should have_many :invoices }
   it { should validate_presence_of :name }
+
+  it "has a valid factory" do
+    merchant = create(:merchant)
+    expect(merchant).to be_valid
+  end
+
+  it "has items" do
+    merchant = create(:merchant_with_items)
+    items    = merchant.items
+    item     = items.first
+
+    expect(items.count).to eq(5)
+    expect(item).to be_valid
+  end
+
 end
