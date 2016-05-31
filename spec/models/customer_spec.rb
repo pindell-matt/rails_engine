d121d414fbe296dbc28e481762c19a87e97775c4
Matt Pindell <pindell.m@gmail.com>
2016-05-31 16:55:13 -0600

    Set up factory for Customer and added to spec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,20 @@ describe Customer do
   it { should have_many :invoices }
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
+
+  it "has a valid factory" do
+    customer = create(:customer)
+    expect(customer).to be_valid
+  end
+
+  it "is invalid without a first_name" do
+    customer = build(:customer, first_name: nil)
+    expect(customer).to_not be_valid
+  end
+
+  it "is invalid without a last_name" do
+    customer = build(:customer, last_name: nil)
+    expect(customer).to_not be_valid
+  end
+
 end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,3 +1,8 @@
 FactoryGirl.define do
 
+  factory :customer do |f|
+    f.first_name "John"
+    f.last_name "Doe"
+  end
+
 end
