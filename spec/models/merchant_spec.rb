14bb747ed013932a85b150af2f79609128e74f80
Matt Pindell <pindell.m@gmail.com>
2016-05-31 15:24:52 -0600

    Refactor Merchant Spec formatting

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Merchant do
-  it { should have_many(:items) }
-  it { should have_many(:invoices) }
-  it { should validate_presence_of(:name) }
+  it { should have_many :items }
+  it { should have_many :invoices }
+  it { should validate_presence_of :name }
 end
