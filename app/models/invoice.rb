fb82b2eb22527a6e97734537de7192f4c76351ee
Matt Pindell <pindell.m@gmail.com>
2016-05-31 16:43:04 -0600

    Refactor Invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,8 @@ class Invoice < ActiveRecord::Base
   belongs_to :merchant
   belongs_to :customer
   has_many   :invoice_items
-
+  
   validates_presence_of :status
+
+  enum status: %w(shipped)
 end
