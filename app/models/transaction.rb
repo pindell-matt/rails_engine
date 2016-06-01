29695e4616a74eff3e419d53b732393b84c58725
Matt Pindell <pindell.m@gmail.com>
2016-05-31 20:27:41 -0600

    Remove Transaction credit card expiration validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ActiveRecord::Base
   belongs_to :invoice
 
   validates_presence_of :credit_card_number
-  validates_presence_of :credit_card_expiration_date
+  # validates_presence_of :credit_card_expiration_date
   validates_presence_of :result
 
   enum status: %w(success failed)
