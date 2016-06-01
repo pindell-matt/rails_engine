c9d25f99b131851207eae3ded02f4119af2d825d
Matt Pindell <pindell.m@gmail.com>
2016-06-01 15:28:07 -0600

    Refactor FindController

--- app/controllers/api/v1/find_controller.rb
+++ app/controllers/api/v1/find_controller.rb
@@ -2,16 +2,16 @@ class Api::V1::FindController < Api::ApiController
   respond_to :json
 
   def index
-    respond_with model_params.where(finder_params)
+    respond_with model_param.where(finder_params)
   end
 
   def show
-    respond_with model_params.find_by(finder_params)
+    respond_with model_param.find_by(finder_params)
   end
 
   private
-    def model_params
-      params["model"].classify.constantize
+    def model_param
+      params.permit("model")["model"].classify.constantize
     end
 
     def finder_params
