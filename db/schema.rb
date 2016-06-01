c057c839f94d4255f3bde3f9aa98948620145297
Matt Pindell <pindell.m@gmail.com>
2016-05-31 20:29:04 -0600

    Import CSV Files (#2)
    * Add csv files to public/data
    
    * Setup import.rake for importing CSV files
    
    * Update csv files to use correct data.
    
    * Enable citext for Merchants and Items
    
    * Remove Transaction credit card expiration validation

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20160531215629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "citext"
 
   create_table "customers", force: :cascade do |t|
     t.string   "first_name"
@@ -47,7 +48,7 @@ ActiveRecord::Schema.define(version: 20160531215629) do
   add_index "invoices", ["merchant_id"], name: "index_invoices_on_merchant_id", using: :btree
 
   create_table "items", force: :cascade do |t|
-    t.string   "name"
+    t.citext   "name"
     t.string   "description"
     t.integer  "unit_price"
     t.integer  "merchant_id"
@@ -58,7 +59,7 @@ ActiveRecord::Schema.define(version: 20160531215629) do
   add_index "items", ["merchant_id"], name: "index_items_on_merchant_id", using: :btree
 
   create_table "merchants", force: :cascade do |t|
-    t.string   "name"
+    t.citext   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 describe Transaction do
   it { should belong_to :invoice }
   it { should validate_presence_of :credit_card_number }
-  it { should validate_presence_of :credit_card_expiration_date }
   it { should validate_presence_of :result }
 end
