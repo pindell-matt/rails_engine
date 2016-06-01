9d9dc89417b093a40caf68834a78e9db238a37cb
Matt Pindell <pindell.m@gmail.com>
2016-06-01 15:28:24 -0600

    Add routes for Customers and Items

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :merchants, only: [:index, :show]
+      resources :customers, only: [:index, :show]
+      resources :items,     only: [:index, :show]
     end
   end
 end
