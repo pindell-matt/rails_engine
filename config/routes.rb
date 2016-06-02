d3b1f50920b578b8e4a6d30ad959539fa16d4b78
Matt Pindell <pindell.m@gmail.com>
2016-06-01 19:46:14 -0600

    Create Transaction and Invoice Item Serializers

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,11 @@ Rails.application.routes.draw do
         end
       end
 
-      resources :customers, only: [:index, :show]
-      resources :items,     only: [:index, :show]
-      resources :invoices,  only: [:index, :show]
+      resources :customers,     only: [:index, :show]
+      resources :items,         only: [:index, :show]
+      resources :invoices,      only: [:index, :show]
+      resources :invoice_items, only: [:index, :show]
+      resources :transactions,  only: [:index, :show]
     end
   end
 end
