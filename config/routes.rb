e59f3685bf32aab09c015c1d4f6f01435b2aa9c2
Matt Pindell <pindell.m@gmail.com>
2016-06-01 22:47:19 -0600

    Add nested resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-
   namespace :api do
     namespace :v1, defaults: { format: :json } do
+
       get '/:model/find',     to: 'find#show'
       get '/:model/find_all', to: 'find#index'
       get '/:model/random',   to: 'random#show'
-    end
-  end
-
-  namespace :api do
-    namespace :v1, defaults: { format: :json } do
 
       resources :merchants, only: [:index, :show] do
         scope module: 'merchants' do
@@ -20,11 +15,53 @@ Rails.application.routes.draw do
         end
       end
 
-      resources :customers,     only: [:index, :show]
-      resources :items,         only: [:index, :show]
-      resources :invoices,      only: [:index, :show]
-      resources :invoice_items, only: [:index, :show]
-      resources :transactions,  only: [:index, :show]
+      resources :customers, only: [:index, :show] do
+        scope module: 'customers' do
+          member do
+            get '/invoices',     to: 'invoices#index'
+            get '/transactions', to: 'transactions#index'
+          end
+        end
+      end
+
+      resources :items, only: [:index, :show] do
+        scope module: 'items' do
+          member do
+            get '/invoice_items', to: 'invoice_items#index'
+            get '/merchant',      to: 'merchants#show'
+          end
+        end
+      end
+
+      resources :invoices, only: [:index, :show] do
+        scope module: 'invoices' do
+          member do
+            get '/transactions',  to: 'transactions#index'
+            get '/invoice_items', to: 'invoice_items#index'
+            get '/items',         to: 'items#index'
+            get '/customer',      to: 'customers#show'
+            get '/merchant',      to: 'merchants#show'
+          end
+        end
+      end
+
+      resources :invoice_items, only: [:index, :show] do
+        scope module: 'invoice_items' do
+          member do
+            get '/invoice', to: 'invoices#show'
+            get '/item',    to: 'items#show'
+          end
+        end
+      end
+
+      resources :transactions,  only: [:index, :show] do
+        scope module: 'transactions' do
+          member do
+            get '/invoice', to: 'invoices#show'
+          end
+        end
+      end
+
     end
   end
 end
