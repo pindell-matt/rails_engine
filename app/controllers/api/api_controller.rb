00cab303ad101658532c5e6e0dc9c7560f862ffb
Matt Pindell <pindell.m@gmail.com>
2016-06-01 22:45:50 -0600

    Add ApiController strong_params to Merchant::controllers

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,3 +1,8 @@
 class Api::ApiController < ApplicationController
   protect_from_forgery with: :null_session
+  helper_method :strong_params
+
+  def strong_params
+    params.require("id")
+  end
 end
--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -2,11 +2,6 @@ class Api::V1::Merchants::ItemsController < Api::ApiController
   respond_to :json
 
   def index
-    respond_with Merchant.find(merchant_params).items.reverse
+    respond_with Merchant.find(strong_params).items.reverse
   end
-
-  private
-    def merchant_params
-      params.require("id")
-    end
 end
