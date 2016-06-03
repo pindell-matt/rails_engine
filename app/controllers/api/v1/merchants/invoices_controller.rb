4686de5db29d73e77e0e8fa5b907fa24440c89dd
Matt Pindell <pindell.m@gmail.com>
2016-06-03 00:25:24 -0600

    Add favorite_customer_controller

--- app/controllers/api/v1/merchants/invoices_controller.rb
+++ app/controllers/api/v1/merchants/invoices_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Merchants::InvoicesController < Api::ApiController
   respond_to :json
 
   def index
-    respond_with Merchant.find(strong_params).invoices.reverse
+    respond_with Merchant.find(strong_params).invoices
   end
 end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -40,6 +40,16 @@ FactoryGirl.define do
         create_list(:item, evaluator.items_count, merchant: merchant)
       end
     end
+
+    factory :merchant_with_invoices do
+      transient do
+        invoices_count 3
+      end
+
+      after(:create) do |merchant, evaluator|
+        create_list(:invoice, evaluator.invoices_count, merchant: merchant)
+      end
+    end
   end
 
   factory :invoice do
