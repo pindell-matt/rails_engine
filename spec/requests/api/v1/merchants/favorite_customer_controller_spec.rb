39bb0e981e836fce8c825d1f10b252724a441a76
Matt Pindell <pindell.m@gmail.com>
2016-06-03 01:43:27 -0600

    Add invoice_items::invoice_controller spec

--- spec/requests/api/v1/merchants/favorite_customer_controller_spec.rb
+++ spec/requests/api/v1/merchants/favorite_customer_controller_spec.rb
@@ -12,20 +12,6 @@ RSpec.describe "Merchants::FavoriteCustomerController" do
       get "/api/v1/merchants/#{@merchant.id}/favorite_customer"
       expect(response).to have_http_status(:success)
     end
-
-    # it "returns the merchant's favorite customer" do
-    #   get "/api/v1/merchants/#{@merchant.id}/favorite_customer"
-    #
-    #   results   = JSON.parse(body)
-    #   require "pry"
-    #   binding.pry
-    #   cust_json = results.first
-    #   expect(cust_json).to eq({
-    #     'id'         => @customer.id,
-    #     'first_name' => @customer.first_name,
-    #     'last_name'  => @customer.last_name
-    #   })
-    # end
   end
 
 end
